37e5ae3c8c67688dc8ae49de1692b2acc700b707
worksamp <work.samp01@gmail.com>
2017-02-22 11:23:23 +0000

    Testing Feb 22

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -2,7 +2,7 @@ package org.eclipse.che.examples;
 
 public class HelloWorld {
     public static void main(String[] argvs) {
-        String a = "Che";
+        String a = "Testing";
         System.out.println("Hello World " + a + "!");
     }
 }
